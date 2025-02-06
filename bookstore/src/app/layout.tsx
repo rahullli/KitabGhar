c07819a3b3f5b48a0c3c046e724c59754887a60a
Rahullli <khandelwalr213@gmail.com>
2025-02-06 23:38:54 +0530

    Added state management.

--- bookstore/src/app/layout.tsx
+++ bookstore/src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
+  display: "swap",  
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={roboto_mono.className}
       >
         {children}
       </body>
