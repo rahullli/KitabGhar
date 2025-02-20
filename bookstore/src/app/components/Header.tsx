60ae220c74c9c08c441de2464759808b4dc4fc8e
Rahullli <khandelwalr213@gmail.com>
2025-02-20 23:57:19 +0530

    Added Shadcn components.

--- bookstore/src/app/components/Header.tsx
+++ bookstore/src/app/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -8,8 +10,15 @@ const Header = () => {
         {/* Desktop Header */}
         <div className='container w-[80%] mx-auto hidden lg:flex items-center justify-between p-4'>
             <Link href="/" className='flex items-center'>
-            <Image src="" width={450} height={450} alt='desktop_logo' className='h-15 w-auto'/>
+            <Image src="" width={450} height={100} alt='desktop_logo' className='h-15 w-auto'/>
             </Link>
+
+            <div className='flex flex-1 items-center justify-center max-w-xl px-4'>
+              <div className='relative w-full'>
+                <Input type='text' placeholder='Book Name / Author / Subject / Publisher' className='w-full pr-10'/>
+                <Button size='icon' variant="ghost" className='absolute right-0 top-1/2 -translate-y-1/2'/>
+              </div>
+            </div>
         </div>
     </header>
   )
