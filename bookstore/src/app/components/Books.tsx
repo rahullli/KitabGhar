8869985dc9ee140af2c36598562e670173a9866c
Rahullli <khandelwalr213@gmail.com>
2025-03-30 23:04:39 +0530

    Added Footer.

--- bookstore/src/app/components/Books.tsx
+++ bookstore/src/app/components/Books.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Books = () => {
   return (
-    <div>Books</div>
+    <section className='py-16 bg-gray-50'>
+        <div className='container mx-auto px-4'>
+            <h2 className = "text-3xl font-bold text-center mb-12">Newly Added Books</h2>
+        </div>
+    </section>
   )
 }
 
--- bookstore/src/app/components/Footer.tsx
+++ bookstore/src/app/components/Footer.tsx
@@ -1,8 +1,39 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-gray-900 text-gray-300'>
+      <div className='container mx-auto px-4 py-12'>
+        <div className='grid gap-12 md:grid-cols-4'>
+          <div>
+            <h3 className='mb-4 text-lg font-semibold text-white'>ABOUT US</h3>
+            <ul className='space-y-2'>
+              <li className=''>
+                <Link href="/about-us">About us</Link>
+              </li>
+              <li className=''>
+                <Link href="/contact-us">Contact us</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h3 className='mb-4 text-lg font-semibold text-white'>USEFUL LINKS</h3>
+            <ul className='space-y-2'>
+              <li className=''>
+                <Link href="/how-it-works">How it works</Link>
+              </li>
+              <li className=''>
+                <Link href="/blogs">Blogs</Link>
+              </li>
+            </ul>
+          </div>
+
+
+        </div>
+      </div>
+    </footer>
   )
 }
 
