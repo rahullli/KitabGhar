93eae0b93c7cd773da4f12e615da10d7eef8551e
Rahullli <khandelwalr213@gmail.com>
2025-05-12 23:38:16 +0530

    Added More content in Footer.

--- bookstore/src/app/components/Footer.tsx
+++ bookstore/src/app/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,11 +10,11 @@ const Footer = () => {
           <div>
             <h3 className='mb-4 text-lg font-semibold text-white'>ABOUT US</h3>
             <ul className='space-y-2'>
-              <li className=''>
-                <Link href="/about-us">About us</Link>
+              <li className='cursor-pointer'>
+                <Link href="/about-us" className='hover:text-white'>About us</Link>
               </li>
-              <li className=''>
-                <Link href="/contact-us">Contact us</Link>
+              <li className='cursor-pointer'>
+                <Link href="/contact-us" className='hover:text-white'>Contact us</Link>
               </li>
             </ul>
           </div>
@@ -21,17 +22,52 @@ const Footer = () => {
           <div>
             <h3 className='mb-4 text-lg font-semibold text-white'>USEFUL LINKS</h3>
             <ul className='space-y-2'>
-              <li className=''>
-                <Link href="/how-it-works">How it works</Link>
+              <li className='cursor-pointer'>
+                <Link href="/how-it-works" className='hover:text-white'>How it works</Link>
               </li>
-              <li className=''>
-                <Link href="/blogs">Blogs</Link>
+              <li className='cursor-pointer'>
+                <Link href="/blogs" className='hover:text-white'>Blogs</Link>
               </li>
             </ul>
           </div>
 
 
+          <div>
+            <h3 className='mb-4 text-lg font-semibold text-white'>POLICIES</h3>
+            <ul className='space-y-2'>
+              <li className='cursor-pointer'>
+                <Link href="/how-it-works" className='hover:text-white'>Terms of Use</Link>
+              </li>
+              <li className='cursor-pointer'>
+                <Link href="/blogs" className='hover:text-white'>Blogs</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h3 className='mb-4 text-lg font-semibold text-white'>STAY CONNECTED</h3>
+            <div className='mb-4 flex space-x-4'>
+                <Link href="#" className='hover: text-white'>
+                   <Facebook className='h-6 w-6'/>
+                </Link>
+                <Link href="#" className='hover: text-white'>
+                   <Instagram className='h-6 w-6'/>
+                </Link>
+                <Link href="#" className='hover: text-white'>
+                   <Youtube className='h-6 w-6'/>
+                </Link>
+                <Link href="#" className='hover: text-white'>
+                   <Twitter className='h-6 w-6'/>
+                </Link>
+            </div>
+
+            <p className='text-sm'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ducimus nostrum adipisci facilis quia totam tempore repudiandae consequatur fugit asperiores accusamus esse illum modi laudantium, quos maiores alias excepturi beatae.
+            </p>
+          </div>
         </div>
+
+        {/* Feature Section */}
       </div>
     </footer>
   )
